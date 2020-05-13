9799dd78f59d85d0a792fc2aff96f5842cfabcbf
jeffk713 <52447909+jeffk713@users.noreply.github.com>
2020-05-12 21:05:37 -0700

    created and styled list item with sample data on history page

--- src/component/Pages/HistoryPage/HistoryPage.component.jsx
+++ src/component/Pages/HistoryPage/HistoryPage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import DATA from './data';
 
-import {ListGroup} from 'react-bootstrap'
+import { Jumbotron, ListGroup, Badge } from 'react-bootstrap'
 
 class HistoryPage extends React.Component {
   constructor() {
@@ -14,11 +14,36 @@ class HistoryPage extends React.Component {
   render() {
     return (
       <div>
-        <ListGroup>
-          <ListGroup.Item variant="primary">
-            Primary
-          </ListGroup.Item>
-        </ListGroup>
+        <Jumbotron
+          className='col-8 mx-auto'
+          style={{ 
+            padding: '40px, 50px',  
+            paddingTop: '40px',
+            marginTop: '30px',
+            minHeight: '80vh' 
+          }}
+        >
+          <ListGroup>
+            <ListGroup.Item>
+              <div
+                style={{
+                  height: '25px', 
+                  display: 'flex',
+                  alignItems: 'center',
+                  paddingLeft: '25px' 
+                }}
+              >
+                <Badge variant="success" style={{ height: '130%' }}>
+                  <h5>SYS: 123&ensp;|&ensp;DIA: 234</h5>
+                </Badge>
+                <div style={{ lineHeight: '0', marginTop: '-7px', marginLeft: '20px' }}>
+                <h5>Prehypertension</h5>
+                <span>May 12, 2020&ensp;|&ensp;67 bpm</span>
+                </div>
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Jumbotron>
       </div>
     )
   }
