046ac64a57273d40b334f9da532ed86480b04977
jeffk713 <52447909+jeffk713@users.noreply.github.com>
2020-05-14 11:33:06 -0700

    created StatChart component and added it to stat page

--- src/component/Pages/StatisticsPage/Statistics.component.jsx
+++ src/component/Pages/StatisticsPage/Statistics.component.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import Container from '../../Container/Container.component';
 import MeasurementGauge from '../../Gauge/Gauge.component';
-
-import { BarChart } from 'react-easy-chart';
+import StatChart from '../../Statistic-chart/Statistic-chart.component';
 
 class StatisticsPage extends React.Component {
   constructor() {
@@ -12,7 +11,12 @@ class StatisticsPage extends React.Component {
     this.state = { 
       sys:136, 
       dia: 87, 
-      bpm: 70 
+      bpm: 70,
+      hypo: 10,
+      nor: 20,
+      pre: 50,
+      st1: 20,
+      st2: 10 
     }
   }
   render() {
@@ -23,23 +27,7 @@ class StatisticsPage extends React.Component {
           <MeasurementGauge value={this.state.dia} label='AVG.DIA' color={'#36A2EB'} />
           <MeasurementGauge value={this.state.bpm} label='AVG.BPM' color={'#FFCE56'} />
         </div>
-        <div>
-          <BarChart 
-            axisLabels={{x: 'Category', y: 'Percentage'}}
-            axes
-            data={[
-            {x: 'Hypotension', y: '23', color:'#7a796a'}, 
-            {x: 'Normal', y: '20', color:'#1a83b8'}, 
-            {x: 'Prehypertension', y: '50', color:'#11960c'}, 
-            {x: 'Stage1 Hypertension', y: '20', color:'#ffe203'},
-            {x: 'Stage2 Hypertension', y: '10', color:'#d92b2b'}
-            ]}
-            yDomainRange={[0, 100]} 
-            height={350}
-            width={650}
-            barWidth={20}
-          />
-        </div>
+        <StatChart {...this.state} />
       </Container>
     )
   }
