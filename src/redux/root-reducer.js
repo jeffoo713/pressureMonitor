213e3a4e6eb889189159f8d25a1e00c3e7756282
jeffk713 <52447909+jeffk713@users.noreply.github.com>
2020-05-15 19:50:36 -0700

    persisted the app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
 import dataReducer from './data/data.reducer';
 import calendarReducer from './calendar/calendar.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['data', 'calendar']
+}
 
 const rootReducer = combineReducers({
   data: dataReducer,
   calendar: calendarReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
