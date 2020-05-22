2348dc8d796d70395e7dc48273f8a00b2c3e950c
jeffk713 <52447909+jeffk713@users.noreply.github.com>
2020-05-22 10:01:14 -0700

    changed list item style

--- src/component/List-item/List-item.component.jsx
+++ src/component/List-item/List-item.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect'
 
 import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
+import Col from 'react-bootstrap/Col';
 
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectDataArr } from '../../redux/data/data.selectors';
@@ -16,21 +17,27 @@ const ListItem = ({ id, sys, dia, bpm, category, colorCode, displayDate, removeD
         height: '25px', 
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-evenly' 
+        justifyContent: 'space-evenly'
       }}
     >
-      <Badge variant={colorCode} style={{ height: '130%' }}>
+      <Col sm={4}>
+        <Badge variant={colorCode} style={{ height: '100%' }}>
         <h5>SYS: {sys}&ensp;|&ensp;DIA: {dia}</h5>
-      </Badge>
-      <div style={{ lineHeight: '0', marginTop: '-7px', marginLeft: '20px' }}>
-      <h5>{category}</h5>
-      <span>{displayDate}&ensp;|&ensp;{bpm} bpm</span>
-      </div>
+        </Badge>
+      </Col>
+      <Col lg={true}>
+        <div style={{ lineHeight: '0', marginTop: '-7px', marginLeft: '20px' }}>
+        <h5>{category}</h5>
+        <span>{displayDate}&ensp;|&ensp;{bpm} bpm</span>
+        </div>
+      </Col>
+      <Col sm={1}>
         <ion-icon 
         name="trash-outline"
         style={{ cursor: 'pointer'}}
         onClick={() => removeDataStart(id, currentUser, currentDataArray)}
         />
+      </Col>
     </div>
   </ListGroup.Item>
 )
